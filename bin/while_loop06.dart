2e850382588963daf17180cafb9c1b583cfb47f4
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-19 15:51:33 +0500

    while_loop06.dart dastur tuzildi

--- bin/while_loop06.dart
+++ bin/while_loop06.dart
@@ -2,4 +2,26 @@
   Create function func with "a " argument
   Return the larger of the odd and even numbers up to 'a', whichever is greater
 */
-void main() {}
+int func(int a) {
+  int i = 0;
+  int summ = 0;
+  int sum = 0;
+  while (i <= a) {
+    if (i % 2 == 1) {
+      sum += i;
+    }
+    if (i % 2 == 0) {
+      summ += i;
+    }
+    i = i += 1;
+  }
+  if (sum < summ) {
+    return summ;
+  } else {
+    return sum;
+  }
+}
+
+void main() {
+  print(func(10));
+}
