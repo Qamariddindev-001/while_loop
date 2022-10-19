280d7c0d4091a1f97fc5bcfd8528ea041448d760
Qamariddindev-001 <qamariddinol664@gmail.com>
2022-10-19 15:54:45 +0500

    while_loop07.dart dastur tuzildi

--- bin/while_loop07.dart
+++ bin/while_loop07.dart
@@ -3,4 +3,26 @@
   Return odd and even numbers up to "a", whichever is smaller
   
 */
-void main() {}
+int func(int a) {
+  int i = 0;
+  int summ = 0;
+  int sum = 0;
+  while (i <= a) {
+    if (i % 2 == 1) {
+      sum += i;
+    }
+    if (i % 2 == 0) {
+      summ += i;
+    }
+    i = i += 1;
+  }
+  if (sum > summ) {
+    return summ;
+  } else {
+    return sum;
+  }
+}
+
+void main() {
+  print(func(10));
+}
