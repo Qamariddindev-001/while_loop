d8d1700a67efbefc4ea1e80f73abb9ee930f97c1
User <qamariddinol664@gmail.com>
2023-01-09 13:16:09 +0500

    while_loop10.dart code

--- bin/while_loop10.dart
+++ bin/while_loop10.dart
@@ -6,4 +6,24 @@
   
   
 */
-void main() {}
+String func(int a) {
+  int x = 0;
+  int y = 0;
+  String i = '';
+  while (x < a) {
+    x++;
+    if (a % x == 0) {
+      y++;
+    }
+  }
+  if (y == 2) {
+    i = 'tup son';
+  } else {
+    i = 'tup son emas';
+  }
+  return i;
+}
+
+void main() {
+  print(func(8));
+}
